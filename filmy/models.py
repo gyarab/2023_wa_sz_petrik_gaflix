0df2ecf7416df174e7d5f6e6529176636cfee08f
Jiriik <jiri.petrik@student.gyarab.cz>
2024-03-22 10:27:15 +0100

    models

--- filmy/models.py
+++ filmy/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=256)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
--- gaflix/urls.py
+++ gaflix/urls.py
@@ -20,7 +20,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', (TemplateView.as_view(template_name='filmy/base.html')))
-]
+    path('', (TemplateView.as_view(template_name='filmy/prvni.html')), name = "prvni"),
+    path('druha/', TemplateView.as_view(template_name='filmy/druha.html'), name = "druhy")
+    ]
 
 
