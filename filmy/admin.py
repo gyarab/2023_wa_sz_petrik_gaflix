1d3ae4a1fdccda47fd9a03f8b6969c29cdd9ddf6
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-05-13 17:52:55 +0200

    updated version

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from filmy.models import Movie, Director, Genre
 
 # Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+class DirectorAdmin(admin.ModelAdmin):
+    pass
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Movie, MovieAdmin)
+
+admin.site.register(Director, DirectorAdmin)
+
+admin.site.register(Genre, GenreAdmin)
--- filmy/models.py
+++ filmy/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
-# Create your models here.
 class Movie(models.Model):
-    title = models.CharField(max_length=256)
+    name = models.CharField(max_length=300)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
+    footage = models.PositiveSmallIntegerField(blank=True, null=True, help_text="in minutes")
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Director(models.Model):
+    name = models.CharField(max_length=300)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
