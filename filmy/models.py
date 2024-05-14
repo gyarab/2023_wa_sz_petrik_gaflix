42dbdeaa41186903d9fdcce568140feadc3761ee
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-05-14 20:27:15 +0200

    added actor

--- filmy/models.py
+++ filmy/models.py
@@ -24,3 +24,12 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+class Actor(models.Model):
+    name = models.CharField(max_length=300)
+    birth_year = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name    
