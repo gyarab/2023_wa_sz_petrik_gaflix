ec2da76e75ea8f33dfda9bacf3d655b5c593fb88
Jiriik <jiri.petrik@student.gyarab.cz>
2024-05-31 09:54:26 +0200

    added movies with desc and pic

--- filmy/views.py
+++ filmy/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Movie, Genre, Actor, Director
 
-# Create your views here.
+def movies(request):
+    context = {"movies" : Movie.objects.all().order_by('name')}
+    return render(request,'filmy/movies.html', context )
--- gaflix/urls.py
+++ gaflix/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', (TemplateView.as_view(template_name='filmy/prvni.html')), name = "prvni"),
-    path('druha/', TemplateView.as_view(template_name='filmy/druha.html'), name = "druhy")
-    ]
+from filmy.views import movies
 
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="filmy/prvni.html")),
+    path("druha/", TemplateView.as_view(template_name="filmy/druha.html")),
+    path("filmy/", movies, name="movies"),
+    #path("film/<int:id>", movie, name="movie"),
+]
 
